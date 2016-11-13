6ad6ccc147285196fc357fa6ba637504860c781e
Jennifer Lubejko <jennifer.lubejko@gmail.com>
2016-11-13 12:01:12 -0600

    Adding CRUD steps learned on Sunday, 11/13

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,51 @@
 class ProductsController < ApplicationController
-  def all_products_method
-    @all_products = Product.all
-    render 'all_products.html.erb'
+  def index
+    @product = Product.all
+    render 'index.html.erb'
+  end
+
+  def new 
+    render 'new.html.erb'
+  end 
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image: params["image"],
+      description: params["description"]
+    )
+    product.save
+    render 'create.html.erb'
+  end 
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render 'show.html.erb'
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render 'edit.html.erb'
+  end
+
+  def update
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.name = params[:name]
+    product.price = params[:price]
+    product.image = params[:image]
+    product.description = params[:description]
+    product.save
+    render 'update.html.erb'
+  end
+
+  def destroy
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.destroy
+    render 'destroy.html.erb'
   end
 end
