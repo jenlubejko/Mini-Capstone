df947c3db94d52b9b78ac1cfa87c6f9efc50367b
Jennifer Lubejko <jennifer.lubejko@gmail.com>
2016-11-16 19:02:01 -0600

    New methods in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+  def products_list
+    products.split(", ")
+  end 
+
+  def sale_message
+    if price.to_f < 9
+      return "Discount Item!"
+    else 
+      return  "Everyday Value!"
+    end
+  end
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
 end
